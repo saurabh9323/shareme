7b1c6e03e274c569e1b558e4515449a75c37a0bf
saurabh592 <shubham.pathak9323@gmail.com>
2023-03-11 10:13:11 +0530

    first commit after project is push on github

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name:"user",
+    title:"user",
+    type:"document",
+    fields:[
+        {
+         name:"username",
+         title:"username",
+         type:"string"   
+        },
+         {
+         name:"image",
+         title:"image",
+         type:"string"   
+        }
+    ]
+}
